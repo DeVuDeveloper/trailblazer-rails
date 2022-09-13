7667e9e94877666be7132c3e916c4b72f12a673a
VuDej <drvu47@gmail.com>
2022-09-13 14:00:52 +0200

    errors, creating user, migration for db and save acc

--- app/concepts/auth/operation/create_account.rb
+++ app/concepts/auth/operation/create_account.rb
@@ -1,14 +1,40 @@
 module Auth::Operation
   class CreateAccount < Trailblazer::Operation
     step :check_email
+    raise :email_invalid_msg, fail_fast: true
     step :passwords_identical?
+    raise :passwords_invalid_msg, fail_fast: true
+    step :password_hash
+    step :state
+    step :save_account
 
-    def check_email(ctx, email:, **)
-      email =~ /\A[^,;@ \r\n]+@[^,@; \r\n]+\.[^,@; \r\n]+\z/ # login_email_regexp, stolen from Rodauth.
+    def check_email(_ctx, email:, **)
+      email =~ /\A[^,;@ \r\n]+@[^,@; \r\n]+\.[^,@; \r\n]+\z/
     end
 
-    def passwords_identical?(ctx, password:, password_confirm:, **)
+    def passwords_identical?(_ctx, password:, password_confirm:, **)
       password == password_confirm
+    end
+
+    def email_invalid_msg(ctx, **)
+      ctx[:error] = 'Email invalid.'
+    end
+
+    def passwords_invalid_msg(ctx, **)
+      ctx[:error] = 'Passwords do not match.'
+    end
+
+    def password_hash(ctx, password:, password_hash_cost: BCrypt::Engine::MIN_COST, **)
+      ctx[:password_hash] = BCrypt::Password.create(password, cost: password_hash_cost)
+    end
+
+    def state(ctx, **)
+      ctx[:state] = 'created, please verify account'
+    end
+
+    def save_account(ctx, email:, password_hash:, state:, **)
+      user = User.create(email:, password: password_hash, state:)
+      ctx[:user] = user
     end
   end
 end
--- test/operations/auth_operation_test.rb
+++ test/operations/auth_operation_test.rb
@@ -6,7 +6,7 @@ class AuthOperationTest < Minitest::Spec
     it 'accepts valid email and passwords' do
       result = Auth::Operation::CreateAccount.wtf?(
         {
-          email: 'drvu@gmail.com',
+          email: 'ar@gmail.com',
           password: 'secret',
           password_confirm: 'secret'
         }
@@ -14,16 +14,27 @@ class AuthOperationTest < Minitest::Spec
 
       assert result.success?
     end
+
     it 'fails on invalid input' do
       result = Auth::Operation::CreateAccount.wtf?(
         {
-          email: 'drvu@gmail',
+          email: 'ar@gmail',
           password: 'secret',
           password_confirm: 'secret'
         }
       )
 
       assert result.failure?
+    end
+    it 'validates input, encrypts the password, and saves user' do
+      result = Auth::Operation::CreateAccount.wtf?()
+
+      assert result.success?
+      user = result[:user]
+      assert user.persisted? 
+      assert_equal 'yogi@trb.to', user.email
+      assert_equal 60, user.password.size
+      ass
     end
   end
 end
