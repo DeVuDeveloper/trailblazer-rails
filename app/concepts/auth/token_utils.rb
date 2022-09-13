9416464bcb63a55e5716f11cc2de0a251049ffb6
VuDej <drvu47@gmail.com>
2022-09-13 18:33:18 +0200

    timing safe comparision against hacker atack added

---
+++ app/concepts/auth/token_utils.rb
@@ -0,0 +1,16 @@
+module Auth
+  module TokenUtils
+    module_function
+
+    private def split_token(token)
+      token.split('_', 2)
+    end
+
+    private
+    
+    def timing_safe_eql?(provided, actual)
+      provided = provided.to_s
+      Rack::Utils.secure_compare(provided.ljust(actual.length), actual) && provided.length == actual.length
+    end
+  end
+end
